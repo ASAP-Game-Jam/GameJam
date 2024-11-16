038603a087f309bed8342c2726d88ae4a87c1d1e
Rudzeris <ruzil1atangulov@gmail.com>
2024-11-17 04:45:41 +1000

    interfaces: modify levelManager, towerAttack

---
+++ Assets/Scripts/Interfaces/ILevelManager.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Scripts.Interfaces
+{
+    public interface ILevelManager
+    {
+        bool IsEnemyOnLine(uint index);
+    }
+}
--- Assets/Scripts/Interfaces/Tower/ITowerAttack.cs
+++ Assets/Scripts/Interfaces/Tower/ITowerAttack.cs
@@ -5,5 +5,7 @@ namespace Assets.Scripts.Interfaces
     public interface ITowerAttack
     {
         event EventHandler OnAttack;
+        event EventHandler OnReloaded;
+        void SetLevelManager(ILevelManager levelManager);
     }
 }
