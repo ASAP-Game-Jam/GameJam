4015ceba94b155788ea53ab595400d99224ae465
rudzeris <ruzil1atangulov@gmail.com>
2024-12-17 23:52:36 +1000

    #3 create tower_spawn_manager

---
+++ Assets/Scripts/Tower/TowerSpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using Assets.Scripts.Interfaces;
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Tower
+{
+    public class TowerSpawnManager : MonoBehaviour, ISpawnerManager
+    {
+        public event EventHandler OnSpawn;
+        public GameObject currentPlant;
+        public Sprite currentPlantSprite;
+        public Transform tiles;
+        public LayerMask tileMask;
+
+        private void Update()
+        {
+            RaycastHit2D hit = Physics2D.Raycast(
+                Camera.main.ScreenToWorldPoint(Input.mousePosition),
+                Vector2.zero,
+                Mathf.Infinity,
+                tileMask
+                );
+
+            foreach(Transform tile in tiles)
+                tile.GetComponent<SpriteRenderer>().enabled = false;
+
+            if(hit.collider && currentPlant)
+            {
+                hit.collider.GetComponent<SpriteRenderer>().sprite = currentPlantSprite;
+                hit.collider.GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+    }
+}
