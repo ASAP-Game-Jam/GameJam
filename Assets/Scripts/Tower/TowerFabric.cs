98addb9a062fd3256d7f61a07f551d6e4db8f8f9
rudzeris <ruzil1atangulov@gmail.com>
2024-12-18 15:01:53 +1000

    modify Tower script

---
+++ Assets/Scripts/Tower/TowerFabric.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Enemy;
+using Assets.Scripts.Interfaces.Tower;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Tower
+{
+    public class TowerFabric : MonoBehaviour, ITowerFabric
+    {
+        public List<TowerTypeToPrefab> objectList = new List<TowerTypeToPrefab>();
+
+        public GameObject GetPrefab(TowerType type)
+        {
+            foreach (var obj in objectList)
+                if (obj.type == type)
+                    return obj.prefab;
+
+            return null;
+        }
+    }
+}
--- Assets/Scripts/Tower/TowerSpawnManager.cs
+++ Assets/Scripts/Tower/TowerSpawnManager.cs
@@ -1,5 +1,9 @@
-﻿using Assets.Scripts.Interfaces;
+﻿using Assets.Scripts.CustomEventArgs;
+using Assets.Scripts.Interfaces;
+using Assets.Scripts.Interfaces.Tower;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts.Tower
@@ -9,8 +13,28 @@ namespace Assets.Scripts.Tower
         public event EventHandler OnSpawn;
         public GameObject currentPlant;
         public Sprite currentPlantSprite;
-        public Transform tiles;
+        public List<Transform> tiles;
         public LayerMask tileMask;
+        private ITowerFabric fabric;
+
+        private void Start()
+        {
+            foreach (var i in FindObjectsOfType<UIButton>())
+                i.OnCardMarked += BuyEntity;
+            fabric = FindObjectOfType<TowerFabric>();
+            tiles = new List<Transform>();
+            foreach (var i in FindObjectsOfType<Cell>())
+                tiles.Add(i.GetComponent<Transform>());
+        }
+
+        private void BuyEntity(object sender, EventArgs args)
+        {
+            if(args is EventMarkedArgs mark)
+            {
+                currentPlant = fabric.GetPrefab(mark.TowerType);
+                currentPlantSprite = currentPlant.GetComponent<Sprite>();
+            }
+        }
 
         private void Update()
         {
@@ -28,6 +52,13 @@ namespace Assets.Scripts.Tower
             {
                 hit.collider.GetComponent<SpriteRenderer>().sprite = currentPlantSprite;
                 hit.collider.GetComponent<SpriteRenderer>().enabled = true;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Instantiate(currentPlant,hit.collider.transform.position, Quaternion.identity);
+                    currentPlant = null;
+                    currentPlantSprite = null;
+                }
             }
         }
     }
