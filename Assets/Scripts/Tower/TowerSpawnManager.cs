9786b2daf23a94a771dcfa89dd6bae6896e9ab50
rudzeris <ruzil1atangulov@gmail.com>
2025-01-04 02:59:47 +1000

    #6 add OnSpawn event

--- Assets/Scripts/Tower/TowerSpawnManager.cs
+++ Assets/Scripts/Tower/TowerSpawnManager.cs
@@ -56,6 +56,7 @@ namespace Assets.Scripts.Tower
                 if (Input.GetMouseButtonDown(0))
                 {
                     Instantiate(currentPlant,hit.collider.transform.position, Quaternion.identity);
+                    OnSpawn?.Invoke(this, EventArgs.Empty);
                     currentPlant = null;
                     currentPlantSprite = null;
                 }
