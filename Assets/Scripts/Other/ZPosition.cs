f76141ec8b4b402430c06ef665cc18e718ede650
Rudzeris <ruzil1atangulov@gmail.com>
2025-06-01 00:49:11 +1000

    Добавил исправление по Z координатам

---
+++ Assets/Scripts/Other/ZPosition.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ZPosition : MonoBehaviour
+{
+    public float minZ = 0.0f;
+    public float coefficient = 100.0f;
+    void Update()
+    {
+        Vector3 vector3 = transform.position;
+        vector3.z = (minZ + vector3.y) / coefficient;
+        transform.position = vector3;
+    }
+}
